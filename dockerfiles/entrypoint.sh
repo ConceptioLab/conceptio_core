70f578c255ac1f1859ccda7eccf1d571d5c6deca
Matheus V. C. Cogo <mvccogo@gmail.com>
2023-10-17 11:05:02 -0300

    Test workflow

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 . /opt/ros/${ROS_DISTRO}/setup.sh
 . /opt/conceptio/conceptio_core/conceptio_interfaces/geographic_info/geographic_msgs/install/setup.sh
 . /opt/conceptio/conceptio_core/conceptio_interfaces/install/setup.sh
