de65b85a74d95acdc4ddabe5a79dced6c2be1240
mvccogo <mvccogo@gmail.com>
2023-11-22 15:25:33 -0300

    Fix variable definition in healthcheck

--- dockerfiles/healthcheck.sh
+++ dockerfiles/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-node_name_entity_management = "/entity_management_node"
-
+node_name_entity_management="/entity_management_node"
+source /opt/ros/$ROS_DISTRO/setup.bash
 node_list=$(ros2 node list)
 
 if [[ $node_list == *"$node_name_entity_management"*]]; then
