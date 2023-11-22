1b6f98b1ce6ff13ea49f76d6c4b36b1a52c6c714
mvccogo <mvccogo@gmail.com>
2023-11-22 15:40:51 -0300

    Add whitespace to end of the statement

--- dockerfiles/healthcheck.sh
+++ dockerfiles/healthcheck.sh
@@ -4,7 +4,7 @@ node_name_entity_management="/entity_management_node"
 source /opt/ros/$ROS_DISTRO/setup.bash
 node_list=$(ros2 node list)
 
-if [[ $node_list == *"$node_name_entity_management"*]]; then
+if [[ "$node_list" == *"$node_name_entity_management"* ]]; then
     exit 0
 else
     exit 1
